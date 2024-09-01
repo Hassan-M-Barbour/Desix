472a18b26ccc6e2c243d6fedc309d503fcfe8bd3
Hassan Barbour <barbour.hassan@gmail.com>
2024-09-01 12:33:55 +0300

    Pages SEO v2

--- pages/en/index.vue
+++ pages/en/index.vue
@@ -1,3 +1,15 @@
+<script setup>
+
+useHead({
+  title: 'ROSETECH | Empower your business',
+  meta: [
+    { name: 'description', content: 'Discover ROSETECH, a leader in software development, where innovation meets excellence. Transform your ideas into seamless digital experiences with our expert team. Client-centric, secure, and proven success.' },
+    { name: 'keywords', content: 'Software development company, Custom software solutions, Digital experience transformation, Innovative software development, Experienced software developers, Client-centric software services, Business software solutions, Software development expertise, Cutting-edge software technology, Secure software development, Proven success in software development, ROSETECH software solutions, Leading software developers, Custom software development services, Digital innovation for businesses' },
+    { name: 'title', content: 'ROSETECH | Empower your business' }
+  ]
+})
+</script>
+
 <template>
     <SectionsHome1Slider/>
     <!-- <SectionsHome1Service/> -->
